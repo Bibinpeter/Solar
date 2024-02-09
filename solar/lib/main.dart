957b0623bc1df9c5e86fb669c237d1a0f07185b9
bibin peter <bibinpeter2018@gmail.com>
2024-02-09 12:07:18 +0530

    first commit

---
+++ solar/lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:solar/models/screens/splash_screen.dart';
+ 
+
+void main() {
+  runApp(const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SplashScreen(),
+    )
+  );
+}
+
---
+++ solar/lib/models/screens/splash_screen.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:solar/constants/constants.dart';
+import 'package:solar/models/screens/homescreen.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+              fit: BoxFit.cover,
+              colorFilter: ColorFilter.mode(
+                  Colors.black54, BlendMode.colorBurn),
+              image: AssetImage(kSplashImage)
+          ),
+        ),
+        child: imageText(context),
+      ),
+    );
+  }
+
+  Widget imageText(context) {
+    return Container(
+      width: double.infinity,
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text('EXPLORE', style: GoogleFonts.exo2(
+              textStyle: const TextStyle(
+                  fontSize: 35,
+                  color: kWhiteColor
+              )
+          )),
+          Text('SOLAR', style: GoogleFonts.exo2(
+              textStyle: const TextStyle(
+                  fontSize: 35,
+                  color: kWhiteColor
+              )
+          )),
+          Text('SYSTEM', style: GoogleFonts.exo2(
+              textStyle: const TextStyle(
+                  fontSize: 35,
+                  color: kWhiteColor
+              )
+          )),
+          const SizedBox(height: 10),
+          CupertinoButton(
+              minSize: 0,
+              padding: EdgeInsets.zero,
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => HomeScreen())
+                );
+              },
+              child: const Icon(Icons.arrow_forward, size: 30, color: Colors.white30)),
+          const SizedBox(height: 120),
+        ],
+      ),
+    );
+  }
+}
